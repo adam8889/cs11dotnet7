07eb21c9ead5f6374bddf97157f29d93baa8eec6
Adam Lumm <alumm07@gmail.com>
2023-04-06 06:56:44 -0400

    complete though page 224

---
+++ Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+Console.WriteLine($"Writing to: {logPath}");
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+//text write is buffered, so this option calls Flush() on all listeners after writing
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Debug says, I am watching");
+Trace.WriteLine("Trace says, I am watching");
+
+Console.WriteLine("Reading from appsettings.json in {0}", arg0: Directory.GetCurrentDirectory());
+
+ConfigurationBuilder builder = new();
+
+builder.SetBasePath(Directory.GetCurrentDirectory());
+
+builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(displayName: "PacktSwitch", description: "This switch is set via a JSON config.");
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+
+Trace.WriteLineIf(ts.TraceError, "TraceError");
+Trace.WriteLineIf(ts.TraceWarning, "TraceWarning");
+Trace.WriteLineIf(ts.TraceInfo, "TraceInformation");
+Trace.WriteLineIf(ts.TraceVerbose, "TraceVerbose");
+
+Console.ReadLine();
