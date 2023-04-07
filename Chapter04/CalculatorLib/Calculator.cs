edccdfcfb0197337cd5d44d14fc1d721cab2e79c
Adam Lumm <alumm07@gmail.com>
2023-04-07 06:47:03 -0400

    Complete through pg 234

---
+++ Chapter04/CalculatorLib/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            //return a * b; //intentional bug
+            return a + b; //bug fixed
+        }
+
+    }
+}
