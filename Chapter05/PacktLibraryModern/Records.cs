121897a2e9737c794cb4442d6b9be008ad2181bd
Adam Lumm <alumm07@gmail.com>
2023-04-30 20:27:12 -0400

    Complete chapter 5, through page 292

---
+++ Chapter05/PacktLibraryModern/Records.cs
@@ -0,0 +1,14 @@
+﻿using Packt.Shared;
+
+public class ImmutablePerson
+{
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+}
+
+public record ImmutableVehicle
+{
+    public int Wheels { get; init; }
+    public string? Color { get; init; }
+    public string? Brand { get; init; }
+}
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -193,3 +193,47 @@ catch (Exception ex)
 {
     WriteLine($"{ex.GetType()} says: {ex.Message} number was {number}.");
 }
+
+
+Passenger[] passengers =
+{
+    new FirstClassPassenger { AirMiles = 1_419, Name = "Suman" },
+    new FirstClassPassenger { AirMiles = 16_562, Name = "Lucy" },
+    new BusinessClassPassenger { Name = "Janice" },
+    new CoachClassPassenger { CarryOnKG = 25.7, Name = "Dave" },
+    new CoachClassPassenger { CarryOnKG = 0, Name = "Amit" }
+};
+
+foreach (Passenger passenger in passengers)
+{
+    decimal flightCost = passenger switch
+    {
+        FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+        FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+        FirstClassPassenger _ => 2000M,
+        BusinessClassPassenger _ => 1000M,
+        CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+        CoachClassPassenger _ => 650M,
+        _ => 800M
+    };
+    WriteLine($"Flight costs {flightCost:C} for {passenger}");
+}
+
+
+ImmutablePerson jeff = new()
+{
+    FirstName = "Jeff",
+    LastName = "Winger"
+};
+//jeff.FirstName = "Geoff";
+
+ImmutableVehicle car = new()
+{
+    Brand = "Mazda MX-5 RF",
+    Color = "Soul Red Crystal Metallic",
+    Wheels = 4
+};
+
+ImmutableVehicle repaintedCar = car with { Color = "Polymetal Grey Metallic" };
+WriteLine($"Original car color was {car.Color}.");
+WriteLine($"New car color is {repaintedCar.Color}.");
