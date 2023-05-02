20b62002ff055cadcfc144e9833208e2bc7ffba7
Adam Lumm <alumm07@gmail.com>
2023-05-01 20:45:15 -0400

    complete through pg 303

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace Packt.Shared;
+
+public class Person : object
+{
+    //properties
+    public string? Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    //methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    //delegate field
+    public event EventHandler? Shout;
+
+    //data field
+    public int AngerLevel;
+
+    //method
+    public void Poke()
+    {
+        AngerLevel++;
+
+        if(AngerLevel >= 3)
+        {
+            //if something is listening...
+            if(Shout != null)
+            {
+                //then call the delegate
+                Shout(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using Packt.Shared;
+
+Person harry = new()
+{
+    Name = "Harry",
+    DateOfBirth = new(year: 2001, month: 3, day: 25)
+};
+
+harry.WriteToConsole();
+
+//non-generic lookup collection
+System.Collections.Hashtable lookupObject = new();
+
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2; //lookup value that has 2 as its key
+
+WriteLine(format: "Key {0} has value: {1}",
+        arg0: key,
+        arg1: lookupObject[key]);
+
+WriteLine(format: "Key {0} has value: {1}",
+        arg0: harry,
+        arg1: lookupObject[harry]);
+
+//generic lookup collection
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+//lookupIntString.Add(key: harry, value: "Delta"); //compile error
+lookupIntString.Add(key: 4, value: "Delta");
+
+//assign a method to the Shout delegate
+harry.Shout += Harry_Shout;
+harry.Shout += Harry_Shout2;
+
+//call the Poke method that raises the Shout event
+harry.Poke();
+harry.Poke();
+harry.Poke();
+harry.Poke();
